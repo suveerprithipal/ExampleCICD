df79c14e5a71d987b76af8765cb0322928268ba1
Suveer Prithipal <suveerprithipal@Suveer.local>
2017-11-22 07:40:00 +1300

    a more guided integration test

--- src/test/java/nz/co/application/example_app/pineapple/integratinTests/integrationTests/LockIT.java
+++ src/test/java/nz/co/application/example_app/pineapple/integratinTests/integrationTests/LockIT.java
@@ -8,21 +8,22 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LockIT {
-
-
   @Test
   public void openLockIT(){
+
     /*
     An integration test is different from a unit test because there is a dependency on another class.
     We will use the processing and output of function A to test the business logic of function B.
+    So we will use the locksmith class to unlock the lock class.
 
     We will use JUnit to to this.
+    Tests were also written in TDD.
      */
-    // given
+    // given I have a lock
     LockInterface lockInterface = new Lock(5,10);
-    //when
+    //when a locksmith comes along with the following credentials
     LockSmith lockSmith = new LockSmith("John",5, 10);
-    //then
+    //then he can open the lock
     assertThat(lockInterface.openLock(lockSmith.getLockNumber(),lockSmith.getKey(lockSmith.getPin(),lockSmith.getLockNumber())));
   }
 }
